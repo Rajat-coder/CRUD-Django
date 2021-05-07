4974a4bfd60f84e561cdbd2c21dd8874da489829
Rajat Handa <handarajat111@gmail.com>
2021-05-08 00:26:05 +0530

    validate field

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -16,3 +16,19 @@ class PodcastSerializer(serializers.ModelSerializer):
     class Meta:
         model = Podcast
         fields ="__all__"
+
+    def validate_participant(self, data):
+        parti_data = data
+        parti_list = parti_data.split(',')
+        parti_data2 = []
+        if len(parti_list) < 11:
+            for i in parti_list:
+                if len(i) <101:
+                    parti_data2.append(i)
+                else:
+                    raise serializers.ValidationError({"participant": "string cannot be larger than 100 characters"})
+        else:
+            raise serializers.ValidationError({"participant": "maximum of 10 participants possible"})
+
+        return data
+    
